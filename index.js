b85c33a83e39d36658586b5964c9e73b081a0166
勤硕 <qinshuo.lqs@alibaba-inc.com>
2018-02-12 15:03:05 +0800

    fuck forgot pw

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const pj = require('./package.json')
 const config = require('./config.json')
+const fetch = require('isomorphic-fetch')
 const octokit = require('@octokit/rest')()
 
 const dp = pj.dependencies || {}
 const ddp = pj.devDependencies || {}
 
-console.log(dp, ddp)
+console.log(`[${Object.keys(dp).length} dependencies]`)
+Object.keys(dp).forEach((d) => console.log('-', d))
+console.log('\n')
 
 function login (username, password) {
   return new Promise((resolve, reject) => {
     if (!username || !password) {
-      var readline = require('readline')
-      var rl = readline.createInterface(process.stdin, process.stdout)
+      const prompt = require('prompt')
 
-      rl.question('what the username is? ', function(input) {
-        username = input
-
-        rl.question('what the password is? ', function(input) {
-          password = input
-          console.log(username, password)
-
-          rl.close()
-          process.stdin.destroy()
-          resolve()
-        })
+      prompt.message = ''
+      prompt.start()
+      prompt.get([
+        { name: 'username', description: 'Github Usrname' },
+        { name: 'password', hidden: true, replace: '*', description: 'Github Password' }
+      ], (err, result) => {
+        resolve(result)
       })
+    } else {
+      resolve({ username, password })
     }
   }).then(data => {
     octokit.authenticate({
       type: 'basic',
-      username,
-      password
+      username: data.username,
+      password: data.password
     })
   })
 }
@@ -47,13 +47,35 @@ function unstar (owner, repo) {
   })
 }
 
+function findAllRepo () {
+  const registryurl = 'http://registry.cnpmjs.org'
+
+  const ap = Object.keys(dp).map(d => {
+    return new Promise((resolve, reject) => {
+      const url = registryurl + '/' + d
+      fetch(url).then(res => res.json())
+      .then(res => {
+        const matched = (/github.com\/(.+)\/(.+)\./g).exec(res.repository.url)
+        resolve({
+          owner: matched[1],
+          repo: matched[2]
+        })
+      })
+    })
+  })
+
+  return Promise.all(ap)
+}
 
 function main () {
   login(config.username, config.password).then(() => {
-    star('lqs469', 'star-dependencies').then(() => {
-      console.log('STAR ⭐ OVER')
+    findAllRepo().then(data => {
+      data.forEach(r => {
+        star(r.owner, r.repo).then(() => {
+          console.log(`⭐  Star => ${r.repo}(${r.owner})`)
+        })
+      })
     })
   })
 }
-
 main()
